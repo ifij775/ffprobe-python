e731af0785fafb330531abf5676e684573491890
ifij775 <chrisweed@gmail.com>
2020-12-08 19:25:57 -0500

    Create FFFormat.py

---
+++ ffprobe/FFFormat.py
@@ -0,0 +1,19 @@
+"""
+Python wrapper for ffprobe command line tool. ffprobe must exist in the path.
+"""
+
+class FFFormat:
+    """
+    An object representation of the multimedia file format.
+    """
+
+    def __init__(self, data_lines):
+        for line in data_lines:
+            if line.find("TAG:") >= 0:
+                line = line[line.find(":")+1:]
+
+            self.__dict__.update({key: value for key, value, *_ in [line.strip().split('=')]})
+
+    def __repr__(self):
+        template = "<Format: #Streams: {nb_streams}, format: {format_name} ({format_long_name})>"
+        return template.format(**self.__dict__)
