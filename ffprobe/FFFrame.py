ab2af09fa994690760c240fd8d8c95144f77390f
ifij775 <chrisweed@gmail.com>
2020-12-15 12:23:43 -0500

    Update FFFrame.py
    Add variables

--- ffprobe/FFFrame.py
+++ ffprobe/FFFrame.py
@@ -10,6 +10,29 @@ class FFFrame:
     def __init__(self, data_lines):
         for line in data_lines:
             self.__dict__.update({key: value for key, value, *_ in [line.strip().split('=')]})
+    
+    def media_type(self):
+        return self.__dict__['media_type']
+    
+    def stream_index(self):
+        return int(self.__dict__['stream_index'])
+    
+    def is_key_frame(self):
+        return self.__dict__['key_frame']=='1'
+    
+    def pkt_pts(self):
+        return int(self.__dict__['pkt_pts'])
+    
+    def pkt_pts_time(self):
+        return float(self.__dict__['pkt_pts_time'])
+    
+    def pkt_dts(self):
+        return int(self.__dict__['pkt_dts'])
+    
+    def pkt_dts_time(self):
+        return float(self.__dict__['pkt_dts_time'])
+    
+    
 
     def __repr__(self):
         template = "<Frame: #Media-type: {media_type}, Stream-index: {stream_index} ({pkt_pts_time})>"
