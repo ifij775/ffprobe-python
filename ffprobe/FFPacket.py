df4cb63d27b5e43014b0ce325118f86f3b7f4d52
ifij775 <chrisweed@gmail.com>
2020-12-15 08:54:13 -0500

    Update FFPacket.py
    Add stream_index and codec_type

--- ffprobe/FFPacket.py
+++ ffprobe/FFPacket.py
@@ -10,6 +10,12 @@ class FFPacket:
     def __init__(self, data_lines):
         for line in data_lines:
             self.__dict__.update({key: value for key, value, *_ in [line.strip().split('=')]})
+            
+    def codec_type(self):
+        return self.__dict__['codec_type']
+    
+    def stream_index(self):
+        return int(self.__dict__['stream_index'])
 
     def __repr__(self):
         template = "<Packet: #Media-type: {media_type}, Stream-index: {stream_index} ({pts_time})>"
