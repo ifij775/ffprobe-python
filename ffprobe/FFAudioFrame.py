09e286856bbe4ff00e4dfbb2636412b860f8f290
ifij775 <chrisweed@gmail.com>
2020-12-15 17:50:20 -0500

    Create FFAudioFrame.py
    Add FFAudioFrame

---
+++ ffprobe/FFAudioFrame.py
@@ -0,0 +1,12 @@
+import FFFrame.py
+
+class FFAudioFrame(FFFrame):
+    def sample_fmt(self):
+        return self.__dict__['sample_fmt']
+    def nb_samples(self):
+        return int(self.__dict__['nb_samples'])
+    def channels(self):
+        return int(self.__dict__['channels'])
+    def channel_layout(self):
+        return self.__dict__['channel_layout']
+        
