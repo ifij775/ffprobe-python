fd8d0201025addcbf71e33ca52615703d35ade7c
ifij775 <chrisweed@gmail.com>
2020-12-20 12:00:04 -0500

    Update FFAudioFrame.py
    Use _data instead of __dict__

--- ffprobe/FFAudioFrame.py
+++ ffprobe/FFAudioFrame.py
@@ -2,11 +2,10 @@ from ffprobe.FFFrame import FFFrame
 
 class FFAudioFrame(FFFrame):
     def sample_fmt(self):
-        return self.__dict__['sample_fmt']
+        return self._data['sample_fmt']
     def nb_samples(self):
-        return int(self.__dict__['nb_samples'])
+        return int(self._data['nb_samples'])
     def channels(self):
-        return int(self.__dict__['channels'])
+        return int(self._data['channels'])
     def channel_layout(self):
-        return self.__dict__['channel_layout']
-        
+        return self._data['channel_layout']
