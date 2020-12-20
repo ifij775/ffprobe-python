31ec2ec1654c8340661f35249bbc2919b9ba5dea
ifij775 <chrisweed@gmail.com>
2020-12-20 12:08:56 -0500

    Update FFVideoStream.py
    Use _data instead of __dict__

--- ffprobe/FFVideoStream.py
+++ ffprobe/FFVideoStream.py
@@ -7,66 +7,64 @@ from ffprobe.FFStream import FFStream
 
 class FFVideoStream(FFStream):
     def width(self):
-        return int(self.__dict__['width'])
+        return int(self._data['width'])
     
     def height(self):
-        return int(self.__dict__['height'])
+        return int(self._data['height'])
     
     def frame_size(self):
         """
         Returns the pixel frame size as an integer tuple (width,height) if the stream is a video stream.
         Returns None if it is not a video stream.
         """
-        width = int(self.__dict__['width'])
-        height = int(self.__dict__['height'])
-        return (width,height)
+        return (self.width(),self.height())
     
     def coded_width(self):
-        return int(self.__dict__['coded_width'])
+        return int(self._data['coded_width'])
     
     def coded_height(self):
-        return int(self.__dict__['coded_height'])
+        return int(self._data['coded_height'])
     
     def has_b_frames(self):
-        return (self.__dict__['has_b_frames']=='1')
+        return (self._data['has_b_frames']=='1')
     
     def sample_aspect_ratio(self):
-        return self.__dict__['sample_aspect_ratio']
+        return self._data['sample_aspect_ratio']
     
     def display_aspect_ratio(self):
-        return self.__dict__['display_aspect_ratio']
+        return self._data['display_aspect_ratio']
     
     def pixel_format(self):
-        return self.__dict__['pix_fmt']
+        return self._data['pix_fmt']
     
     def level(self):
-        return self.__dict__['level']
+        return self._data['level']
     
     def color_range(self):
-        return self.__dict__['color_range']
+        return self._data['color_range']
     
     def color_space(self):
-        return self.__dict__['color_space']
+        return self._data['color_space']
     
     def color_transfer(self):
-        return self.__dict__['color_transfer']
+        return self._data['color_transfer']
     
     def color_primaries(self):
-        return self.__dict__['color_primaries']
+        return self._data['color_primaries']
     
     def bits_per_raw_sample(self):
-        return int(self.__dict__['bits_per_raw_sample'])
+        return int(self._data['bits_per_raw_sample'])
     
     
     def chroma_location(self):
-        return self.__dict__['chroma_location']
+        return self._data['chroma_location']
     
     def refs(self):
-        return int(self.__dict__['refs'])
+        return int(self._data['refs'])
     
     def is_avc(self):
-        return (self.__dict__['is_avc']=='true')
+        return (self._data['is_avc']=='true')
     
     def nal_length_size(self):
-        return int(self.__dict__['nal_length_size'])
+        return int(self._data['nal_length_size'])
     
